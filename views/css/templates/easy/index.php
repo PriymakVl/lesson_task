d81eae938e9583dc8f4d65e6fa9101f20092fee5
Vladimir <PriymakVl@mail.ru>
2020-04-12 12:37:19 +0300

    change index templates css

--- views/css/templates/easy/index.php
+++ views/css/templates/easy/index.php
@@ -13,35 +13,47 @@
 	</li>
 	<li>
 		<?= Html::a('Active Box Download', ['template', 'level' => 'easy', 'name' => 'active_box_download']) ?>
+		<span class="menu-points">margin, padding</span>
 	</li>
 	<li>
 		<?= Html::a('Mogo Facts', ['template', 'level' => 'easy', 'name' => 'mogo_facts']) ?>
-	</li>
-	<li>
-		<?= Html::a('Mogo Quote', ['template', 'level' => 'easy', 'name' => 'mogo_quote']) ?>
-	</li>
-	<li>
-		<?= Html::a('Mogo Testimonial', ['template', 'level' => 'easy', 'name' => 'mogo_testimonial']) ?>
+		<span class="menu-points">border, google fonts</span>
 	</li>
 	<li>
 		<?= Html::a('Mogo Map', ['template', 'level' => 'easy', 'name' => 'mogo_map']) ?>
+		<span class="menu-points">background-img, selector:after</span>
 	</li>
 	<li>
 		<?= Html::a('Bouncy Team Contact', ['template', 'level' => 'easy', 'name' => 'bouncy_team_contact']) ?>
+		<span class="menu-points">background-img, border-radius</span>
+	</li>
+		<li>
+		<?= Html::a('Vegie Offers', ['template', 'level' => 'easy', 'name' => 'vegie_offers']) ?>
+		<span class="menu-points">background-img, background-repeat</span>
+	</li>
+	</li>
+	<li>
+		<?= Html::a('Mogo Testimonial', ['template', 'level' => 'easy', 'name' => 'mogo_testimonial']) ?>
+		<span class="menu-points">position absolute, position relative</span>
+	</li>
+	<li>
+		<?= Html::a('Mogo Quote', ['template', 'level' => 'easy', 'name' => 'mogo_quote']) ?>
+		<span class="menu-points">position absolute, position relative</span>
 	</li>
 	<li>
 		<?= Html::a('Active Box Features', ['template', 'level' => 'easy', 'name' => 'active_box_features']) ?>
+		<span class="menu-points">float</span>
 	</li>
 	<li>
 		<?= Html::a('Active Box Team', ['template', 'level' => 'easy', 'name' => 'active_box_team']) ?>
+		<span class="menu-points">float</span>
 	</li>
 	<li>
 		<?= Html::a('Active Box Review', ['template', 'level' => 'easy', 'name' => 'active_box_review']) ?>
-	</li>
-	<li>
-		<?= Html::a('Vegie Offers', ['template', 'level' => 'easy', 'name' => 'vegie_offers']) ?>
+		<span class="menu-points">dots, opacity</span>
 	</li>
 	<li>
 		<?= Html::a('Change Background', ['template', 'level' => 'easy', 'name' => 'change_background']) ?>
+		<span class="menu-points">hover, transition, overflow</span>
 	</li>
 </ol>
